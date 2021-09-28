fc8b77807925389cd5f072f2d02280105fa6da6d
Juseung Park <juseung92@gmail.com>
2021-09-28 16:51:36 -0600

    Go file refactored to use Gin

--- services/users/main.go
+++ services/users/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
-func handler(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+var ginLambda *ginadapter.GinLambda
+
+func init() {
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Gin cold start")
+	r := gin.Default()
+
+	users := r.Group("users")
+
+	users.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	ginLambda = ginadapter.New(r)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
-	lambda.Start(handler)
+	lambda.Start(Handler)
 }
