52317e45d5c362552e119f52ebd47f00021a7734
Juseung Park <juseung92@gmail.com>
2021-10-08 12:24:08 -0600

    feature: added authorization for the http api

--- cdk/lib/api-gateway-stack.ts
+++ cdk/lib/api-gateway-stack.ts
@@ -1,27 +1,68 @@
 import { Construct, Stack, StackProps } from "@aws-cdk/core";
-import { HttpApi, PayloadFormatVersion } from "@aws-cdk/aws-apigatewayv2";
+import {
+  HttpApi,
+  IHttpRouteAuthorizer,
+  PayloadFormatVersion,
+} from "@aws-cdk/aws-apigatewayv2";
 import { LambdaProxyIntegration } from "@aws-cdk/aws-apigatewayv2-integrations";
 import { GoFunction } from "@aws-cdk/aws-lambda-go";
 import * as path from "path";
+import { HttpJwtAuthorizer } from "@aws-cdk/aws-apigatewayv2-authorizers";
+
+/**
+ * Rest Api Options
+ * @extends StackProps
+ * @property {string} identityProvider    - Name of the identity provider
+ * @property {string} identityIssuerUrl   - URL for the identity issuer
+ * @property {string[]} identityAudience  - List of the identity audiences
+ */
+interface RestApiStackProps extends StackProps {
+  /**
+   * @example <caption>Name of the identity provider</caption>
+   * { identityProvider: "okta" }
+   */
+  identityProvider: string;
+  /**
+   * @example <caption>URL for the identity issuer</caption>
+   * { identityIssuerUrl: "https://dev-1234.okta.com/oauth2/default" }
+   */
+  identityIssuerUrl: string;
+  /**
+   * @example <caption>List of the identity audiences</caption>
+   * { identityProvider: ["api://default"] }
+   */
+  identityAudience: string[];
+}
 
 export class RestApiStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: RestApiStackProps) {
     super(scope, id, props);
 
-    const api = new HttpApi(this, `${id}-stack`, {
+    const api = new HttpApi(this, `${id}-http-api`, {
       corsPreflight: {
-        allowOrigins: ["*"],
+        // Only allow https protocol
+        allowOrigins: ["https://*"],
       },
     });
 
+    const authorizer = new HttpJwtAuthorizer({
+      authorizerName: props.identityProvider,
+      jwtIssuer: props.identityIssuerUrl,
+      jwtAudience: props.identityAudience,
+    });
+
     // User endpoint
-    this.addService(api, "users");
+    this.addService(api, "users", { authorizer, scopes: ["user:read"] });
 
     // Organization endpoint
     // this.addService(api, "organizations");
   }
 
-  addService(api: HttpApi, name: string) {
+  addService(
+    api: HttpApi,
+    name: string,
+    auth?: { authorizer: IHttpRouteAuthorizer; scopes: string[] }
+  ) {
     const serviceLambda = new GoFunction(this, `${name}-service`, {
       entry: path.join(__dirname, `../../services/${name}`),
     });
@@ -29,13 +70,19 @@ export class RestApiStack extends Stack {
       handler: serviceLambda,
       payloadFormatVersion: PayloadFormatVersion.VERSION_1_0,
     });
+    // proxy route
     api.addRoutes({
       path: `/${name}/{proxy+}`,
       integration: serviceIntegration,
+      authorizer: auth?.authorizer,
+      authorizationScopes: auth?.scopes,
     });
+    // root route
     api.addRoutes({
       path: `/${name}`,
       integration: serviceIntegration,
+      authorizer: auth?.authorizer,
+      authorizationScopes: auth?.scopes,
     });
   }
 }
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,11 +1,24 @@
 import * as cdk from "@aws-cdk/core";
+import { config } from "dotenv";
 import { RestApiStack } from "./api-gateway-stack";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    new RestApiStack(this, "rest-api", props);
+    config();
+
+    const { IDENTITY_PROVIDER, IDENTITY_ISSUER_URL, IDENTITY_AUDIENCE } =
+      process.env;
+
+    // Create API Stack
+    new RestApiStack(this, "app-name", {
+      identityProvider: IDENTITY_PROVIDER!,
+      identityIssuerUrl: IDENTITY_ISSUER_URL!,
+      identityAudience: JSON.parse(IDENTITY_AUDIENCE!),
+      ...props,
+    });
+
+    // DynamoDB
   }
 }
